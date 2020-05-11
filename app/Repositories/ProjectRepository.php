a7253a037cde26919c75c57742a88d201c03507f
Halat <halat@tut.by>
2020-05-11 16:45:00 +0300

    project controller was return on working version

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -25,29 +25,36 @@ class ProjectRepository extends CoreRepository
         }
 
         $res = $this->startConditions()->
+        leftJoin('employees', 'projects.manager_id', '=', 'employees.id')->
         leftJoin('tasks', 'projects.id', '=', 'tasks.project_id')->
         leftJoin('employees_tasks', 'tasks.id', '=', 'employees_tasks.id')->
         select(
             DB::raw('count(employees_tasks.task_id) as task_count'),
-            'projects.*'
+            'projects.*',
+            'employees.last_name',
+            'employees.first_name',
+            'employees.second_name'
         )->
-        with('employee:id,last_name,first_name,second_name')->
         groupBy('projects.id')->
         orderBy($order)->
         paginate();
+
         return $res;
     }
 
     public function getSearchResult($str)
     {
         $res = $this->startConditions()->
+        leftJoin('employees', 'projects.manager_id', '=', 'employees.id')->
         leftJoin('tasks', 'projects.id', '=', 'tasks.project_id')->
         leftJoin('employees_tasks', 'tasks.id', '=', 'employees_tasks.id')->
         select(
             DB::raw('count(employees_tasks.task_id) as task_count'),
-            'projects.*'
+            'projects.*',
+            'employees.last_name',
+            'employees.first_name',
+            'employees.second_name'
         )->
-        with('employee:id,last_name,first_name,second_name')->
         where('projects.title', 'like', '%' . $str . '%')->
         orWhere('projects.alias', 'like', '%' . $str . '%')->
         orWhere('projects.description', 'like', '%' . $str . '%')->
